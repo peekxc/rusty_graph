0887e06cc60d97da56e23dc861888f59a2387609
peekxc <matt.piekenbrock@gmail.com>
2022-11-19 14:28:26 -0500

    augmenting the function to accept positions apriori

--- src/rusty_graph/lib.rs
+++ src/rusty_graph/lib.rs
@@ -1,16 +1,19 @@
-use std::vec::Vec;
-// use pyo3::prelude::*;
+// use std::vec::Vec;
+use pyo3::wrap_pyfunction;
+use pyo3::prelude::*;
+
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
-use force_graph::{ForceGraph, Node, NodeData};
-use ndarray;
-use numpy::{IntoPyArray, ToPyArray, PyArray, PyArray1, PyArray2, PyArrayDyn, PyReadonlyArrayDyn};
+// use force_graph::{ForceGraph, Node, NodeData};
+use ndarray::{ArrayViewMutD}; //ArrayD, ArrayViewD, 
+// use numpy::{IntoPyArray, ToPyArray, PyArray, PyArray1, PyArray2, PyArrayDyn, PyReadonlyArrayDyn};
+use numpy::{PyArrayDyn};
 //https://docs.rs/numpy/latest/numpy/
 
 
 fn fr(z: f64, k: f64) -> f64 { (k * k) / z }            // repulsive force
 fn fa(z: f64, k: f64, x: f64) -> f64 { (x * x) / k }    // repulsive attraction 
 
-fn update_positions(mut pos_x: Vec<f64>, mut pos_y: Vec<f64>, iterations: i32) -> PyArrayDyn< f64 > {
+fn update_positions(mut pos_x: PyArrayDyn<f64>, mut pos_y: PyArrayDyn<f64>, iterations: i32) -> bool {
     // Implementing psuedo code from paper, image attached in repo
     // I should move these parameters somewhere else but placing for here now
     let W: f64 = 100.0;
@@ -19,6 +22,9 @@ fn update_positions(mut pos_x: Vec<f64>, mut pos_y: Vec<f64>, iterations: i32) -
     let num_nodes: usize = pos_x.len();
     let mut k = area / num_nodes as f64;
     k = k.sqrt();
+
+    let X: ArrayViewMutD< f64 > = ArrayViewMutD::From(pos_x);
+    let Y: ArrayViewMutD< f64 > = ArrayViewMutD::From(pos_y);
 
     let mut disp = [(0.0, 0.0); 5];
 
@@ -29,8 +35,8 @@ fn update_positions(mut pos_x: Vec<f64>, mut pos_y: Vec<f64>, iterations: i32) -
                 println!("v, u = {}, {}", v, u);
                 if v != u {
                     let mut delta = (
-                        pos_x[v] - pos_x[u],
-                        pos_y[v] - pos_y[u]
+                        X[v] - X[u],
+                        Y[v] - Y[u]
                     );
                     let delta_size = (delta.0 * delta.0 + delta.1 * delta.1).sqrt();
                     disp[v].0 = disp[v].0 + (delta.0 / delta_size) * fr(delta_size, k);
@@ -40,42 +46,34 @@ fn update_positions(mut pos_x: Vec<f64>, mut pos_y: Vec<f64>, iterations: i32) -
             }
         }
     }
-
-    pos_x[0].0 = 5.0;
-
+    return true
 }
 
 // https://itnext.io/how-to-bind-python-numpy-with-rust-ndarray-2efa5717ed21
 // https://doc.rust-lang.org/std/vec/struct.Vec.html 
-#[pyfunction]
-fn create_force_graph(a: usize, b: usize) -> PyArrayDyn< f64 > { //Vec< f64 > {
-    // create a force graph with default parameters
-    let mut graph: ForceGraph = <ForceGraph>::new(Default::default());
-    let xc: Vec<f64> = vec![250.0, 750.0, 250.0, 750.0, 500.0];
-    let yc: Vec<f64>= vec![250.0, 250.0, 750.0, 750.0, 500.0];
+// #[pyfunction]
+// fn create_force_graph(a: usize, b: usize) -> PyArrayDyn< f64 > { //Vec< f64 > {
+//     // create a force graph with default parameters
+//     let mut graph: ForceGraph = <ForceGraph>::new(Default::default());
+//     let xc: Vec<f64> = vec![250.0, 750.0, 250.0, 750.0, 500.0];
+//     let yc: Vec<f64>= vec![250.0, 250.0, 750.0, 750.0, 500.0];
 
-    // https://docs.rs/force_graph/latest/force_graph/struct.NodeData.html
-    let mut NI = Vec::new();
-    for i: usize in 1..5 {
-        let n_idx: NodeIndex = graph.add_node(NodeData { x: xc[i] as f32, y: yc[i] as f32, ..Default::default() });
-        NI.push(n_idx);
-    }
-    graph.add_edge(NI[0], NI[2], Default::default());
-    // XC
-    let array = ndarray::Array::eye(a);
-    // array.into_pyarray()
-}
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+//     // https://docs.rs/force_graph/latest/force_graph/struct.NodeData.html
+//     let mut NI = Vec::new();
+//     for i in 1..5 {
+//         let n_idx: NodeIndex = graph.add_node(NodeData { x: xc[i] as f32, y: yc[i] as f32, ..Default::default() });
+//         NI.push(n_idx);
+//     }
+//     graph.add_edge(NI[0], NI[2], Default::default());
+//     // XC
+//     let array = ndarray::Array::eye(a);
+//     return array.into_pyarray()
+// }
 
 /// A Python module implemented in Rust.
+/// pip install --no-build-isolation --no-deps --editable .
 #[pymodule]
 fn rg(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(create_force_graph, m)?)?;
+    m.add_function(wrap_pyfunction!(update_positions, m)?)?;
     Ok(())
 }
